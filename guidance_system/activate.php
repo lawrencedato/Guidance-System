90bfe1c827a40cfc384bdf142f388301e84a4140
Tricia Ann Cruz <cruz_triciaann@plpasig.edu.ph>
2026-05-13 13:44:41 +0800

    change link rel

--- guidance_system/activate.php
+++ guidance_system/activate.php
@@ -173,7 +173,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UNITYCARE | Account Activation</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
     <style>
         .modal-overlay {
             position: fixed;
